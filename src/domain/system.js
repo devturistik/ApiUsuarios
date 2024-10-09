995435e9f456bcf5816220df065725661fb0ea4a
Carlos Arriagada <93371413+carlos-arriagadaCL@users.noreply.github.com>
2024-10-09 13:44:52 -0300

    Estructura de la entidad Sistema

---
+++ src/domain/system.js
@@ -0,0 +1,8 @@
+// src/domain/system.js
+export default class System {
+  constructor(id, nombre, descripcion) {
+    this.id = id; // ID del sistema
+    this.nombre = nombre; // Nombre del sistema
+    this.descripcion = descripcion; // Descripción del sistema
+  }
+}
